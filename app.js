e92ab5dc5c3b67b93aabce610d6fe17458998aa6
kep21 <89517626+kep21@users.noreply.github.com>
2021-09-06 23:36:02 +0300

    Added remain function

--- app.js
+++ app.js
@@ -9,38 +9,76 @@ function mathOperations(type) {
 	}
 
 	switch (type) {
-	case 'plus':
-	answerElement.innerHTML = firstNumber + secondNumber;
-	break;
-	case 'minus':
-	answerElement.innerHTML = firstNumber - secondNumber;
-	break;
-	case 'multiply':
-	answerElement.innerHTML = firstNumber * secondNumber;
-	break;
-	case 'divideup':
-	if (secondNumber !== 0) {
-	answerElement.innerHTML = firstNumber / secondNumber;
+		case 'plus':
+			answerElement.innerHTML = firstNumber + secondNumber;
+			break;
+		case 'minus':
+			answerElement.innerHTML = firstNumber - secondNumber;
+			break;
+		case 'multiply':
+			answerElement.innerHTML = firstNumber * secondNumber;
+			break;
+		case 'divideup':
+			if (secondNumber !== 0) {
+				answerElement.innerHTML = firstNumber / secondNumber;
 			}
-	answerElement.innerHTML = "Can't be divided by zero!";
-	break;
+			answerElement.innerHTML = "Can't be divided by zero!";
+			break;
+	}
+}
+
+function whatcountry() {
+	const answerElement = document.getElementById('out');
+	const country = document.getElementById('country').value;
+
+	switch (country) {
+		case 'Ukraine':
+			answerElement.innerHTML = 'Привіт';
+			break;
+		case 'German':
+			answerElement.innerHTML = 'Hallo';
+			break;
+		case 'English':
+			answerElement.innerHTML = 'Hello';
+			break;
+		case 'French':
+			answerElement.innerHTML = 'Bonjour';
+			break;
+		case 'Turkey':
+			answerElement.innerHTML = 'Merhaba';
+			break;
+		case 'China':
+			answerElement.innerHTML = '你好';
+			break;
+		default:
+			alert('No matches found');
+	}
+}
+
+
+function incrementer() {
+	var i = document.getElementById("add");
+	i.value++;
+}
+
+
+function decrement() {
+	var i = document.getElementById("add");
+	i.value--;
+}
+
+function remaind() {
+	const inputValue = document.getElementById("add").value
+
+	if (typeof inputValue !== 'number') {
+		console.log('Opps')
+		return;
 	}
 
-}
- function whatcountry(type) {
- const answerElement = document.getElementById('out');
-
- switch (type) {
-	case 'Ukraine':
- answerElement.innerHTML = ('Привіт');
-	break;
- case 'Poland':
-	answerElement.innerHTML = ('Hej');
-	break;
-}
-}
-
-function increment () {
-	var i = getElementById('add')
-	i.value++;
+	const result = inputValue % 3;
+	if (result === 0) {
+		console.log('YES!');
+	} else {
+		console.log('NO');
+	}
 }
