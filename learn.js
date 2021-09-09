6277937f8cb35cca68786335cff0ea9593ff1786
kep21 <89517626+kep21@users.noreply.github.com>
2021-09-09 17:25:15 +0300

    learn.js
    2

---
+++ learn.js
@@ -0,0 +1,15 @@
+const friends = [
+	{ name: "Oleg", age: 20, city: "Lviv", gender: "Male",yearofBirth:"2000" },
+	{ name: "Ira", age: 67, city: "Kyiv", gender: "Female", yearofBirth:"2003" },
+	{ name: "Max", age: 32, city: "Yavoriv", gender: "Male", yearofBirth:"1975" },
+	{ name: "Olya", age: 19, city: "Lviv", gender: "Female", yearofBirth:"1988" },
+	{ name: "Ivan", age: 21, city: "Odessa", gender: "Male", yearofBirth:"1980" },
+	{ name: "Roman", age: 24, city: "Dnipro", gender: "Male",yearofBirth:"1995"}
+];
+
+console.log(friends)
+
+/*
+	1 - Вивести тільки тих друзів, які з міста Львів
+	2 - Вивести масив друзів, який був початково + додати поле - рік народження { ..., yearOfBirth: 2000 , ... }
+*/
