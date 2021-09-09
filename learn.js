fb65997127ca0e35388dda33e8bad7ec884dd23f
Yura Husak <yura.husak@juriba.com>
2021-09-09 18:32:09 +0300

    Difference and filter

--- learn.js
+++ learn.js
@@ -1,19 +1,19 @@
 const friends = [
-	{ name: "Oleg", age: 20, city: "Lviv", gender: "Male" },
-	{ name: "Ira", age: 67, city: "Kyiv", gender: "Female" },
-	{ name: "Max", age: 32, city: "Yavoriv", gender: "Male" },
-	{ name: "Olya", age: 19, city: "Lviv", gender: "Female" },
-	{ name: "Ivan", age: 21, city: "Odessa", gender: "Male" },
-	{ name: "Roman", age: 24, city: "Dnipro", gender: "Male" }
+	{ name: "Oleg", age: 20, city: "Lviv", gender: "Male", firstSalary: 1200, secondSalary: 3000 },
+	{ name: "Ira", age: 67, city: "Kyiv", gender: "Female", firstSalary: 1600, secondSalary: 14000 },
+	{ name: "Max", age: 32, city: "Yavoriv", gender: "Male", firstSalary: 900, secondSalary: 13000 },
+	{ name: "Olya", age: 19, city: "Lviv", gender: "Female", firstSalary: 7200, secondSalary: 90000 },
+	{ name: "Ivan", age: 21, city: "Odessa", gender: "Male", firstSalary: 1600, secondSalary: 6000 },
+	{ name: "Roman", age: 24, city: "Dnipro", gender: "Male", firstSalary: 2550, secondSalary: 8000 }
 ];
 
-function calculateBirth(array) {
-	const currentYear = new Date().getFullYear();
-	return array.map(friend => {
-		friend.yearOfBirth = currentYear - friend.age;
-		return friend;
-	});
+/*
+	Показати тільки тих друзів, які проживають у Львові та Києві. Додати нове пока - різниця між другою та першою зарплатою. Нехай поле називається difference.
+*/
+
+function newFriends(array) {
+	return array;
 }
 
 
-console.log(calculateBirth(friends))
+console.log(newFriends(friends));
