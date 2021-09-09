218d4addc227b837168f527e14ce321626ba03c4
Yura Husak <yura.husak@juriba.com>
2021-09-09 18:09:45 +0300

    Add yearOfBirth property

--- learn.js
+++ learn.js
@@ -1,15 +1,19 @@
 const friends = [
-	{ name: "Oleg", age: 20, city: "Lviv", gender: "Male",yearofBirth:"2000" },
-	{ name: "Ira", age: 67, city: "Kyiv", gender: "Female", yearofBirth:"2003" },
-	{ name: "Max", age: 32, city: "Yavoriv", gender: "Male", yearofBirth:"1975" },
-	{ name: "Olya", age: 19, city: "Lviv", gender: "Female", yearofBirth:"1988" },
-	{ name: "Ivan", age: 21, city: "Odessa", gender: "Male", yearofBirth:"1980" },
-	{ name: "Roman", age: 24, city: "Dnipro", gender: "Male",yearofBirth:"1995"}
+	{ name: "Oleg", age: 20, city: "Lviv", gender: "Male" },
+	{ name: "Ira", age: 67, city: "Kyiv", gender: "Female" },
+	{ name: "Max", age: 32, city: "Yavoriv", gender: "Male" },
+	{ name: "Olya", age: 19, city: "Lviv", gender: "Female" },
+	{ name: "Ivan", age: 21, city: "Odessa", gender: "Male" },
+	{ name: "Roman", age: 24, city: "Dnipro", gender: "Male" }
 ];
 
-console.log(friends)
+function calculateBirth(array) {
+	const currentYear = new Date().getFullYear();
+	return array.map(friend => {
+		friend.yearOfBirth = currentYear - friend.age;
+		return friend;
+	});
+}
 
-/*
-	1 - Вивести тільки тих друзів, які з міста Львів
-	2 - Вивести масив друзів, який був початково + додати поле - рік народження { ..., yearOfBirth: 2000 , ... }
-*/
+
+console.log(calculateBirth(friends))
