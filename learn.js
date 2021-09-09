2a302c4e55b960c94218085df7b29ea95b155865
kep21 <89517626+kep21@users.noreply.github.com>
2021-09-09 15:52:19 +0300

    learn.js
    ?

--- learn.js
+++ learn.js
@@ -11,5 +11,8 @@
 
 	const first = [1, 2, 3];
 	const second = [4, 5, 6];
-	const combined=[]. concat(first, second);
-	console.log(combined)
+	const result = {}
+	for(var i = 0; i < first.length; i++) {
+    result[first[i]] = second[i];
+}
+console.log(result)
